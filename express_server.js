5602bb89ab8095bc869d4eca519b69b7cb08d95b
Mikhail Serebriakov <mishacyb@gmail.com>
2020-05-14 03:52:41 +0000

    Code refactor, comments made more informative

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
@@ -45,6 +45,7 @@ function generateRandomString() {
   return result;
 }
 
+// Checks if the user with given an email exists in the database
 function emailExists(email) {
   for (let user in users) {
     if (users[user].email === email) {
@@ -54,6 +55,7 @@ function emailExists(email) {
   return false;
 }
 
+// Returns a user from the database given an email
 function findUser(email) {
   for (let user in users) {
     if (users[user].email === email) {
@@ -62,6 +64,7 @@ function findUser(email) {
   }
 }
 
+// Returns urls belonging to the user with a given id
 function urlsForUser(id) {
   const result = {};
   for (let url in urlDatabase) {
@@ -74,53 +77,60 @@ function urlsForUser(id) {
 
 /* 
 * GET /
-* Redirect to /urls page
+* If a user is logged in, redirect to /urls page
+* If a user is not logged in, redirect to /login page
 */
 app.get("/", (req, res) => {
   const user = users[req.cookies.user_id];
-  if (!user) {
-    res.redirect("/login");
-  } else {
+  if (user) {
     res.redirect("/urls");
+  } else {
+    res.redirect("/login");
   }
 });
 
 /*  
 * GET /urls
 * BROWSE
-* Display all entries in the database
+* If a user is logged in, display all entries in the database
+* If a user is not logged in, send an error message
 */
 app.get("/urls", (req, res) => {
   const user = users[req.cookies.user_id];
-  if (!user) {
-    res.send("Please register or login first!");
-  } else {
+  if (user) {
     let templateVars = {
       user: user,
       urls: urlsForUser(user.id),
     };
     res.render("urls_index", templateVars);
+  } else {
+    res.status(401).send("Error 401: please register or login first\n");
   }
-  
 });
 
 /* 
 * POST /urls
 * ADD
-* Add new entry to the database
+* If a user is logged in, add a new entry to the database
+* If a user is not logged in, send an error message
 */
 app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString();
-  urlDatabase[shortURL] = {};
-  urlDatabase[shortURL].longURL = req.body.longURL;
-  urlDatabase[shortURL].userID = req.cookies.user_id;
-  res.redirect(`/urls/${shortURL}`);
-  console.log(urlDatabase);
+  const user = users[req.cookies.user_id];
+  if (user) {
+    const shortURL = generateRandomString();
+    urlDatabase[shortURL] = {};
+    urlDatabase[shortURL].longURL = req.body.longURL;
+    urlDatabase[shortURL].userID = req.cookies.user_id;
+    res.redirect(`/urls/${shortURL}`);
+  } else {
+    res.status(403).send("Error 403: access denied, cannot add a new entry while not logged in\n");
+  }
 });
 
 /* 
 * GET /urls/new
-* Render a page containing a form for creating new entries
+* If a user is logged in, render a page containing a form for creating new entries
+* If a user is not logged in, send an error message
 */
 app.get("/urls/new", (req, res) => {
   const user = users[req.cookies.user_id];
@@ -132,32 +142,38 @@ app.get("/urls/new", (req, res) => {
   } else {
     res.redirect("/login");
   }
-  
 });
 
 /* 
 * GET /urls/:shortURL
 * READ
-* Show a particular entry of the database
+* If a user is logged in, show the particular entry
+* If the entry does not exist, send an error message
+* If a user is nog logged in, send an error message
 */
 app.get("/urls/:shortURL", (req, res) => {
   const user = users[req.cookies.user_id];
-  if (user && user.id === urlDatabase[req.params.shortURL].userID) {
-    let templateVars = {
-      user: user,
-      shortURL: req.params.shortURL,
-      longURL: urlDatabase[req.params.shortURL].longURL,
-    };
-    res.render("urls_show", templateVars);
+  if (urlDatabase[req.params.shortURL]) {
+    if (user && user.id === urlDatabase[req.params.shortURL].userID) {
+      let templateVars = {
+        user: user,
+        shortURL: req.params.shortURL,
+        longURL: urlDatabase[req.params.shortURL].longURL,
+      };
+      res.render("urls_show", templateVars);
+    } else {
+      res.status(403).send("Error 403: access denied, cannot show url\n");
+    }
   } else {
-    res.send("Access denied, can't open url");
+    res.status(404).send("Error 404: page not found\n");
   }
 });
 
 /*
 * POST /urls/:shortURL/delete
 * DELETE
-* Delete a particular entry in the database
+* If a user is logged in, delete the particular entry
+* If a user is not logged in, send an error message
 */
 app.post("/urls/:shortURL/delete", (req, res) => {
   const user = users[req.cookies.user_id];
@@ -165,8 +181,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
     delete urlDatabase[req.params.shortURL];
     res.redirect("/urls");
   } else {
-    res.status(401);
-    res.send("Access denied, can't delete url");
+    res.status(403).send("Error 403: access denied, cannot delete the url\n");
   }
   
 });
@@ -174,7 +189,8 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 /*
 * POST /urls/:shortURL
 * EDIT
-* Edit a particular entry in the databse
+* If a user is logged in, edit the particular entry
+* If a user is not logged in, send an error message
 */
 app.post("/urls/:shortURL", (req, res) => {
   const user = users[req.cookies.user_id];
@@ -182,76 +198,95 @@ app.post("/urls/:shortURL", (req, res) => {
     urlDatabase[req.params.shortURL].longURL = req.body.newURL;
     res.redirect("/urls");
   } else {
-    res.status(401);
-    res.send("Access denied, can't edit url");
+    res.status(403).send("Error 403: access denied, cannot edit the url\n");
   }
-  
 });
 
 /* 
 * GET /u/:shortURL
-* Redirect to a long URL which corresponds to a passed short URL
+* Redirect to the long URL which corresponds to the passed short URL
 */
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL].longURL; 
-  res.redirect(longURL);
-});
-
-app.get("/login", (req, res) => {
-  res.render("user_login");
-});
-
-/* 
-* POST /login
-* 
-*/
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  if (emailExists(email)) {
-    const user = findUser(email);
-    if (user.password === password) {
-      res.cookie("user_id", user.id);
-      res.redirect("/urls");
-    } else {
-      res.status(403);
-      res.send("Error 403 Wrong password");
-    }
+  if (urlDatabase[req.params.shortURL]) {
+    const longURL = urlDatabase[req.params.shortURL].longURL; 
+    res.redirect(longURL);
   } else {
-    res.status(403);
-    res.send("Error 403 User with this email not found");
+    res.status(404).send("Error 404: url does not exist\n");
   }
 });
 
 /*
+* GET /login
+* If a user is logged in, redirect to /urls
+* If a user is not logged in, render login page
+*/
+app.get("/login", (req, res) => {
+  const user = users[req.cookies.user_id];
+  if (user) {
+    res.redirect("/urls");
+  } else {
+    res.render("user_login");
+  }
+});
+
+/* 
+* POST /login
+* LOGIN
+* Logs the user in
+* If the email provided does not exist, send an error message
+* If the password does not match with the provided email, send an error message
+*/
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    if (emailExists(email)) {
+      const user = findUser(email);
+      if (user.password === password) {
+        res.cookie("user_id", user.id);
+        res.redirect("/urls");
+      } else {
+        res.status(403);
+        res.send("Error 403: wrong password\n");
+      }
+    } else {
+      res.status(403);
+      res.send("Error 403: user with this email not found\n");
+    }
+});
+
+/*
 * POST /logout
-* 
+* LOGOUT
+* Clear the cookie "user_id" and redirect to /urls
 */
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
-  res.redirect("/login");
+  res.redirect("/urls");
 });
 
 /*
 * GET /register
-* Open user registration form
+* If a user is logged in, redirect to /urls
+* If a user is not logged in, open user registration form
 */
 app.get("/register", (req, res) => {
   const user = users[req.cookies.user_id];
-  let templateVars = {
-    user: user
-  };
-  res.render("user_register", templateVars);
+  if (user) {
+    res.redirect("/urls");
+  } else {
+    res.render("user_register");
+  }
 });
 
 /*
 * POST /register
-* Register a new user
+* REGISTER
+* If any of the fields is empty, send an error message
+* If the entered email is already taken, send an error message
 */
 app.post("/register", (req, res) => {
   const { email, password } = req.body;
   if (email === "" || password === "") {
-    res.status(400);
-    res.send("Error 400 Some fields are empty");
+    res.status(400).send("Error 400: some fields are empty\n");
   } else {
     if (!emailExists(email)) {
       const id = generateRandomString();
@@ -263,24 +298,10 @@ app.post("/register", (req, res) => {
       res.cookie("user_id", id);
       res.redirect("/urls");
     } else {
-      res.status(400);
-      res.send("Error 400 Email already taken");
+      res.status(400).send("Error 400: email already taken\n");
     }
   }
   console.log(users);
-});
-
-/*
-Routes for testing purposes
-*/
-
-// Get json version of a database
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
